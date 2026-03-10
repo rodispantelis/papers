c9d67417157e325872fa0270ce5f8ca30845779b
Pantelis Rodis <32998445+rodispantelis@users.noreply.github.com>
2026-03-10 09:33:06 +0200

    Add files via upload

---
+++ apps/mosaiko.js
@@ -0,0 +1,13 @@
+mos=[
+//[link_to_the_map, path_to_the_icon, name],
+["https://play.google.com/store/apps/details?id=com.rodis.datacollectionad","icons/datacollection.png", "Data collection"],
+["https://play.google.com/store/apps/details?id=com.mapthisway","icons/mapthisway.png", "Map This Way"],
+["https://wms-viewer-online.appspot.com/","icons/wmsmapviewer.png", "WMS Map Viewer"],
+["https://rodispantelis.github.io/WMSMapGallery/","icons/wmsmapgallery.png", "WMS Map Gallery"],
+["https://github.com/rodispantelis/tools","icons/readbigfiles.png", "readBIGfiles"],
+["https://wms-viewer-online.appspot.com/coor/coor.html","icons/geocoordinates.png","Geographical Coordinates"],
+["https://wms-viewer-online.appspot.com/random/randomgraphs.html","icons/randomgraphs.png","Random Graphs"],
+["https://play.google.com/store/apps/details?id=com.randomsequencesfree","icons/rnandroid.png","Random Numbers Android"],
+["https://wms-viewer-online.appspot.com/random/random.html","icons/randomnumbers.jpg","Random Numbers web"],
+["https://play.google.com/store/apps/details?id=com.passrating","icons/passwordrating.png","Password Rating"]
+]; 
